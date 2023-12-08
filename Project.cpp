f0912c76d6e8b70867faa46ff60e5d227e385344
Raheel Dars <150416971+RaheelDars@users.noreply.github.com>
2023-12-08 22:44:05 +0500

    Update Project.cpp

--- Project.cpp
+++ Project.cpp
@@ -20,56 +20,53 @@ double const PI =3.14;
 
 int main() {
     int calc,choice,userinput;
-	    cout<<"\n**********Scientific Calculator************"<<endl;
+    cout<<"\n**********Scientific Calculator************"<<endl;
 	
     A:	
-	   cout<<"\nPress 1 For Arthematic operation : "<<endl;
-	   cout<<"Press 2 For Trignomatric : "<<endl;
-	   cout<<"Press 3 For Logarithm : "<<endl;
-	   cout<<"Press 4 For Power function :\n "<<endl;
-	   cout<<"Enter the operation you want : ";
-	   cin>>userinput;
+      cout<<"\nPress 1 For Arthematic operation : "<<endl;
+      cout<<"Press 2 For Trignomatric : "<<endl;
+      cout<<"Press 3 For Logarithm : "<<endl;
+      cout<<"Press 4 For Power function :\n "<<endl;
+      cout<<"Enter the operation you want : ";
+      cin>>userinput;
 	
  //Arthematic operation
 	 
     if(userinput==1){
- 	    cout<<"1 : Addition "<<endl;
-        cout<<"2 : Subtraction "<<endl;
- 	    cout<<"3 : Multiplication "<<endl;
- 		cout<<"4 : Division "<<endl;
- 		cout<<"5 : Main Menu \n"<<endl;
- 		cout<<"Enter your choice = ";
-     	cin>>choice; 	
-		   if(choice==1){
-		  	    calc=add();
-		  	    goto A;
-            }
-            
-            if(choice==2)
-			{
-		  	    calc=subtract();	
-		  	    goto A;
-            }
-	   
-            if(choice==3)
-			{
-		  	    calc=multiply();
-		  	    goto A;
-            }  
-      
-            if(choice==4)
-		   {
-		  	    calc=division();
-		  	    goto A;
-            }
+ 	      cout<<"1 : Addition "<<endl;
+              cout<<"2 : Subtraction "<<endl;
+ 	      cout<<"3 : Multiplication "<<endl;
+ 	      cout<<"4 : Division "<<endl;
+ 	      cout<<"5 : Main Menu \n"<<endl;
+ 	      cout<<"Enter your choice = ";
+     	      cin>>choice; 
 	    
+	    if(choice==1)
+	    {
+		  calc=add();
+	          goto A;
+             }
+            if(choice==2)
+	    {
+	         calc=subtract();	
+		 goto A;
+            }
+           if(choice==3)
+	    {
+	        calc=multiply();
+	        goto A;
+            }  
+           if(choice==4)
+	    {
+		  calc=division();
+	          goto A;
+            }
             if(choice==5)
-			{
-		  	    goto A;
-            }   
-	    }
-
-		 
+	     {
+		  goto A;
+            
+	     }   
+	       } 
 //Trignomatric operation
     if(userinput==2)
 	{
@@ -83,114 +80,99 @@ int main() {
      	cout<<"Enter your choice = ";
      	cin>>choice;
      	
-   	        if(choice==1) 
-		    {
-		  	    calc=sin();
-				goto A;  	
-            }
-      
-            if(choice==2)
-			{
-		  	    calc=cos();
-				goto A;	
-            }
-	   
-            if(choice==3)
-		    {
-		  	    calc=tan();
-		  	    goto A;
-            }  
-      
-            if(choice==4) 
-		    {
-                calc=sin_inverse();
-                goto A;
-		    }
-	    
-            if(choice==5)
-			{
-		        calc=cos_inverse();
-		        goto A;
-			} 
-	   
-            if(choice==6)
-			{
-		        calc=tan_inverse();
-		        goto A;
-            }  
-      
-            if(choice==7)
-			{
-     	       goto A;
-	        }  	 
-    }
-  
-//Logarithm operation
-	if(userinput==3){
-        cout<<"1 :  Natural log\n";
-        cout<<"2 : Main Menu \n ";
-	    cout<<"Enter your choice = ";
-	  	cin>>choice;
-	  
-	        if(choice==1)
-			{
-	  		    calc=log();  
-				goto A;	
-            }
-           
-            if(choice==2)
-			{
-                goto A;
-		    }
-	    }
-    
-//Power function operation    
-    
-    if(userinput==4){ 		
-        cout<<"1 : For power \n ";
-        cout<<"2 : For square  \n ";
-        cout<<"3 : For square_root  \n ";
-        cout<<"4 : For Cube \n ";
-        cout<<"5 : For Main Menu \n ";
-        cout<<"Enter your choice = ";
-        cin>>choice;
-        
-		    if(choice==1)
-			{
-		   	    calc=power();
-		   	    goto A;
-		    }
-     	   
-     	    if(choice==2)
-			{
-		   	    calc=square();
-		   	    goto A;
-		    }
-		   
-		    if(choice==3)
-			{
-		        calc=square_root();
-		        goto A;
-		    } 
-     	   
-     	    if(choice==4)
-		    {  	
-  		   	    calc=cube();
-  		   	    goto A;
-		    } 
-     	   
-     	    if(choice==5)
-			{
-		   	    goto A ;
-		    }	
-	    }
-	 
-	if(userinput==5)
+   	if(choice==1) 
+         {
+	      calc=sin();
+	      goto A;  	
+         }
+        if(choice==2)
 	{
-	 	return 0;
-	    }
+	     calc=cos();
+	     goto A;	
+        }
+        if(choice==3)
+	 {
+	     calc=tan();
+	     goto A;
+	 }  
+        if(choice==4) 
+	{
+             calc=sin_inverse();
+             goto A;
+	} 
+       if(choice==5)
+	{
+	    calc=cos_inverse();
+	    goto A;
+	} 
+      if(choice==6)
+       {
+	   calc=tan_inverse();
+	   goto A;
+      }  
+     if(choice==7)
+     {
+     	goto A;
+     }  	 
+      }
+  //Logarithm operation
+  if(userinput==3)
+  {
+      cout<<"1 :  Natural log\n";
+      cout<<"2 : Main Menu \n ";
+      cout<<"Enter your choice = ";
+      cin>>choice;
+	  
+     if(choice==1)
+      {
+	  calc=log();  
+	  goto A;	
+      }
+     if(choice==2)
+     {
+        goto A;
+     }
+ }
+//Power function operation      
+if(userinput==4){ 		
+     cout<<"1 : For power \n ";
+     cout<<"2 : For square  \n ";
+     cout<<"3 : For square_root  \n ";
+     cout<<"4 : For Cube \n ";
+     cout<<"5 : For Main Menu \n ";
+     cout<<"Enter your choice = ";
+     cin>>choice;
+     if(choice==1)
+      {
+	  calc=power();
+	  goto A;
+      }
+    if(choice==2)
+     {
+	  calc=square();
+          goto A;
+     }
+    if(choice==3)
+     {
+          calc=square_root();
+	  goto A;
+     } 
+    if(choice==4)
+     {  	
+  	  calc=cube();
+  	  goto A;
+     } 
+    if(choice==5)
+     {
+	 goto A ;
+     }	
+}
+	 
+if(userinput==5)
+ {
+    return 0;
+ }
     }
-
 float add(){
 	int x, y;
 	cout<<"Enter the First number : "<<endl;
@@ -216,23 +198,23 @@ float multiply(){
 	cout<<"Multiplication = "<<x * y;
 }
 float division(){
-	int x, y;
-	cout<<"Enter the First number : ";
-	cin>>x;
-	cout<<"Enter the second number : ";
-	cin>>y;
-    int c=x/y;
-	cout<<"Division = "<<c;
+     int x, y;
+     cout<<"Enter the First number : ";
+     cin>>x;
+     cout<<"Enter the second number : ";
+     cin>>y;
+     int c=x/y;
+     cout<<"Division = "<<c;
 }
 float sin(){
-	double x;
+    double x;
     cout << "Enter angle (in radians) : ";
     cin >> x;
     double result = sin(x);
     cout << "Sin(" << x << ") : " << result << endl;
 }
 float cos(){
-	double x;
+    double x;
     cout << "Enter angle (in radians) : ";
     cin >> x;
     double result = cos(x);
@@ -246,65 +228,65 @@ float tan(){
     cout << "Tan(" << x << ") : " << result << endl;
 }
 float sin_inverse(){
-	double  x ;
-	cout<<"Enter the value (radian) : ";
-	cin>>x;
+    double  x ;
+    cout<<"Enter the value (radian) : ";
+    cin>>x;
     double result ;
     result = asin(x) * (180/PI);
     cout << "asin("<<x<<")  = " << result << " degrees" << endl;
 }
 float cos_inverse(){
-	double  x ;
-	cout<<"Enter the value (radian) : ";
-	cin>>x;
+    double  x ;
+    cout<<"Enter the value (radian) : ";
+    cin>>x;
     double result ;
     result = acos(x) * (180/PI);   
     cout << "acos("<<x<<")  = " << result << " degrees" << endl;	
 }
 float tan_inverse(){
-	double  x ;
-	cout<<"Enter the value (radian) : ";
-	cin>>x;
-    double result ;
-    result = asin(x) * (180/PI);    
-    cout << "atan("<<x<<")  = " << result << " degrees" << endl;
+   double  x ;
+   cout<<"Enter the value (radian) : ";
+   cin>>x;
+   double result ;
+   result = asin(x) * (180/PI);    
+   cout << "atan("<<x<<")  = " << result << " degrees" << endl;
   	
 }
 float log(){
-	int x ;
-	cout<<"Enter the value : ";
-	cin>>x;
-	float result = log (x);
-	cout << "log(x) = " << result << endl;
+    int x ;
+    cout<<"Enter the value : ";
+    cin>>x;
+    float result = log (x);
+    cout << "log(x) = " << result << endl;
 }
 
 float power(){
-	int x, y;
-	cout<<"Enter the number you want to power : ";
-	cin>>x>>y;
-	int res=pow(x,y);
-	cout<<"Power = "<<res;
+   int x, y;
+   cout<<"Enter the number you want to power : ";
+   cin>>x>>y;
+   int res=pow(x,y);
+   cout<<"Power = "<<res;
 }
 float square(){
-	int x;
-	cout<<"Enter the numner you want square : ";
-	cin>>x;
-	int res=pow(x,2);
-	cout<<"Square = "<<res;
+   int x;
+   cout<<"Enter the numner you want square : ";
+   cin>>x;
+   int res=pow(x,2);
+   cout<<"Square = "<<res;
 }
 float cube(){
-	int x;
-	cout<<"Enter the number you want to cube : ";
-	cin>>x;
-	int res=pow(x,3);
-	cout<<"Power = "<<res;
+    int x;
+    cout<<"Enter the number you want to cube : ";
+    cin>>x;
+    int res=pow(x,3);
+    cout<<"Power = "<<res;
 }
 float square_root(){
-	int x;
-	cout<<"Enter the number you want to square_root : ";
-	cin>>x;
-	int res=sqrt(x);
-	cout<<"square_root = "<<res;
+    int x;
+    cout<<"Enter the number you want to square_root : ";
+    cin>>x;
+    int res=sqrt(x);
+    cout<<"square_root = "<<res;
 }
 
 
